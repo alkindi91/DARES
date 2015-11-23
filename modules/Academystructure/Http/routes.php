3d85102bfa521fa53cd69e7873e7de0102aac425
HAitHAm HAmdy <hisambahaa@yahoo.com>
2015-11-23 13:53:57 +0400

    academy structure crud

--- modules/Academystructure/Http/routes.php
+++ modules/Academystructure/Http/routes.php
@@ -3,14 +3,14 @@
 Route::group(['prefix' => 'academystructure', 'namespace' => 'Modules\Academystructure\Http\Controllers'], function()
 {
 	///////////////////faculty/////////////////////////
-
 	Route::get('/',  ['as' =>'faculty.index', 'uses' =>'FacultyController@index']);
+	
 	Route::get('create', ['as' =>'faculty.create', 'uses' =>'FacultyController@create_faculty']);
-
 	post('store', ['as' =>'faculty.store', 'uses' =>'FacultyController@store_faculty']);
 
-	Route::get('edit', ['as' =>'faculty.edit', 'uses' =>'FacultyController@edit_faculty']);
-	Route::get('delete', ['as' =>'faculty.delete', 'uses' =>'FacultyController@delete_faculty']);
-
+	Route::get('edit/{faculty}', ['as' =>'faculty.edit', 'uses' =>'FacultyController@edit_faculty']);
+	post('update/{faculty}', ['as' =>'faculty.update', 'uses' =>'FacultyController@update_faculty']);
+	
+	Route::get('delete/{faculty}', ['as' =>'faculty.delete', 'uses' =>'FacultyController@delete_faculty']);
 	////////////////////Year///////////////////////////
 });
--- modules/Academystructure/Resources/views/faculty/edit.blade.php
+++ modules/Academystructure/Resources/views/faculty/edit.blade.php
@@ -1,10 +1,31 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
+@extends('layouts.master')
 
-<body>
-</body>
-</html>
+@section('content')
+{!! Form::open(['route'=>['faculty.update' ,$faculty->id]  ,'method'=>'POST' , 'class'=>'form-horizontal']) !!}
+<div class="form-group">
+    <div class="col-md-12">
+	{!! Form::label('name', 'اسم الكلية', ['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) !!}
+      <div class="col-md-6 col-sm-6 col-xs-12">
+      {!! Form::text('name', $faculty->name, $attributes = array('class'=>'form-control')) !!}
+      </div>
+    </div>
+</div>
+
+{{-- comment
+<div class="ln_solid"></div>
+    @include('Academystructure::faculties._fields')  
+<div class="ln_solid"></div>
+--}}
+
+<div class="ln_solid"></div>
+<div class="form-group">
+    <div class="col-md-12">
+        <a href="{{ route('faculty.index') }}" class="pull-left btn btn-primary">
+        <i class="fa fa-times"></i> @lang('global.cancel')</a>
+        <button type="submit" class="pull-left btn btn-success">
+        <i class="fa fa-save"></i> @lang('global.save')</button>
+    </div>
+</div>
+
+{!! Form::close() !!}
+@stop
