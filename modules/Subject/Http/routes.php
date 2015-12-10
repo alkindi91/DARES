b8da8ea3ca946b34081c9903f229e44f86c91ff9
ahmed alkindi <alkindi91@gmail.com>
2015-12-10 12:26:28 +0400

    renames files in module Subject

--- modules/Subject/Http/routes.php
+++ modules/Subject/Http/routes.php
@@ -16,21 +16,21 @@ Route::group(['prefix'=>'lessons'] ,function() {
 	get('/{sid}', ['as'=>'lessons.index','uses'=>"LessonsController@index",'middleware'=>'permission:subject.view.lesson']);
 	get('create/{sid}', ['as'=>'lessons.create','uses'=>'LessonsController@create','middleware'=>'permission:subject.create.lesson']);
 	post('store/{sid}', ['as'=>'lessons.store','uses'=>'LessonsController@store','middleware'=>'permission:subject.create.lesson']);
-	get('edit/{slessons}', ['as'=>'lessons.edit','uses'=>'LessonsController@edit','middleware'=>'permission:subject.edit.lesson']);
-	post('update/{slessons}', ['as'=>'lessons.update','uses'=>'LessonsController@update','middleware'=>'permission:subject.edit.lesson']);
-	get('delete/{slessons}', ['as'=>'lessons.delete','uses'=>'LessonsController@delete','middleware'=>'permission:subject.delete.lesson']);
+	get('edit/{sLessons}', ['as'=>'lessons.edit','uses'=>'LessonsController@edit','middleware'=>'permission:subject.edit.lesson']);
+	post('update/{sLessons}', ['as'=>'lessons.update','uses'=>'LessonsController@update','middleware'=>'permission:subject.edit.lesson']);
+	get('delete/{sLessons}', ['as'=>'lessons.delete','uses'=>'LessonsController@delete','middleware'=>'permission:subject.delete.lesson']);
 	
 	post('delete-bulk/{slessons}', ['as'=>'lessons.deleteBulk','uses'=>'LessonsController@deleteBulk','middleware'=>'permission:subject.delete.lesson']);
 	
 });
 Route::group(['prefix'=>'elements'] ,function() {
-    get('/{selements}', ['as'=>'elements.index','uses'=>"ElementsController@index",'middleware'=>'permission:subject.view.element']);
-	get('create/{selements}', ['as'=>'elements.create','uses'=>'ElementsController@create','middleware'=>'permission:subject.create.element']);
-	post('store/{selements}', ['as'=>'elements.store','uses'=>'ElementsController@store','middleware'=>'permission:subject.create.element']);
-	get('edit/{selements}', ['as'=>'elements.edit','uses'=>'ElementsController@edit','middleware'=>'permission:subject.edit.element']);
-	post('update/{selements}', ['as'=>'elements.update','uses'=>'ElementsController@update','middleware'=>'permission:subject.edit.element']);
-	get('delete/{selements}', ['as'=>'elements.delete','uses'=>'ElementsController@delete','middleware'=>'permission:subject.delete.element']);
-	post('delete-bulk/{selements}', ['as'=>'elements.deleteBulk','uses'=>'ElementsController@deleteBulk','middleware'=>'permission:subject.delete.element']);
+    get('/{sElements}', ['as'=>'elements.index','uses'=>"ElementsController@index",'middleware'=>'permission:subject.view.element']);
+	get('create/{sElements}', ['as'=>'elements.create','uses'=>'ElementsController@create','middleware'=>'permission:subject.create.element']);
+	post('store/{sElements}', ['as'=>'elements.store','uses'=>'ElementsController@store','middleware'=>'permission:subject.create.element']);
+	get('edit/{sElements}', ['as'=>'elements.edit','uses'=>'ElementsController@edit','middleware'=>'permission:subject.edit.element']);
+	post('update/{sElements}', ['as'=>'elements.update','uses'=>'ElementsController@update','middleware'=>'permission:subject.edit.element']);
+	get('delete/{sElements}', ['as'=>'elements.delete','uses'=>'ElementsController@delete','middleware'=>'permission:subject.delete.element']);
+	post('delete-bulk/{sElements}', ['as'=>'elements.deleteBulk','uses'=>'ElementsController@deleteBulk','middleware'=>'permission:subject.delete.element']);
 });
 
 
