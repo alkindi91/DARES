3aa2065394557956421e5f67f57f93e282074b2e
ahmed alkindi <alkindi91@gmail.com>
2015-12-17 13:35:32 +0400

    add subjects for each department

--- modules/Academystructure/Http/Controllers/SubjectsController.php
+++ modules/Academystructure/Http/Controllers/SubjectsController.php
@@ -1,12 +1,14 @@
 <?php namespace Modules\Academystructure\Http\Controllers;
 
+use Modules\Subject\Entities\Subject;
 use Pingpong\Modules\Routing\Controller;
 
 class SubjectsController extends Controller {
 	
 	public function index($depid)
 	{
-		return view('academystructure::subjects.index');
+		$subjects=Subject::lists('name','id')->toArray();
+		return view('academystructure::subjects.index',compact('subjects'));
 	}
 	
 }
